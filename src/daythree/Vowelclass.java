a706c9914e51dba7e039e5dd800fadec8f6ee8cc
Abi Ramya <abiramya.m@gmail.com>
2019-08-26 13:03:43 +0530

    updated

--- src/daythree/Vowelclass.java
+++ src/daythree/Vowelclass.java
@@ -1,8 +1,7 @@
 package daythree;
 
-public class Taskthree {
-	
-	public static void main (String[] args) {
+public class Vowelclass {
+public static void main (String[] args) {
 		
 		System.out.println("Day three task by Abiramya");
 		
